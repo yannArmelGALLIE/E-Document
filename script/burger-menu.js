a2c17a96556c0e65a45ec5b3f4dbc838e8c39582
yannArmelGALLIE <professional.gallie@gmail.com>
2025-04-24 19:18:00 +0000

    Creation of the responsive navbar

---
+++ script/burger-menu.js
@@ -0,0 +1,8 @@
+const burgerMenuButton = document.querySelector(".burger-menu-button");
+const burgerMenu = document.querySelector(".burger-menu");
+
+burgerMenuButton.addEventListener("click", () => {
+    burgerMenu.classList.toggle("open")
+    const isOpen = burgerMenu.classList.contains("open")
+    burgerMenuButton.innerHTML = isOpen ? "<span class=\"material-symbols-outlined\">close</span>" : "<span class=\"material-symbols-outlined\">menu</span>"
+})
