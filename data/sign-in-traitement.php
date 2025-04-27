d9c3c06ce866e9856ba912aa1503b5844b042459
yannArmelGALLIE <professional.gallie@gmail.com>
2025-04-27 04:41:09 +0000

    Add of the sign in traitement

--- data/sign-in-traitement.php
+++ data/sign-in-traitement.php
@@ -1,5 +1,16 @@
 <?php 
-if(isset($_POST["submit"])){
-    $numCni = $_POST["numCni"];
+include("db.php");
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $numCni = $_POST["num-cni"];
+    $password = $_POST["password"];
+    if($email != "" && $password != ""){
+        $req = $bdd -> query("SELECT * FROM users WHERE numCni = '$numCni' and password = '$password'");
+        $rep = $req -> fetch();
+        if($rep) {
+            echo "vous êtes connecté";
+        } else {
+            $error_msg = "incorrect";
+        }
+    }
 }
 ?>
